e8456dcae505449407268ee3770cc6ba51b00b5b
kansaraurmil55@gmail.com <urmil.kansara@sap.com>
2017-10-26 20:57:54 -0400

    Final commit

--- ProductInfo/src/com/assignment/model/ProductReturn.java
+++ ProductInfo/src/com/assignment/model/ProductReturn.java
@@ -2,27 +2,29 @@ package com.assignment.model;
 
 import java.io.Serializable;
 
-public class Product implements Serializable{
+public class ProductReturn implements Serializable{
 	private static final long serialVersionUID = 7526472295622776147L;
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
 	private int rfqId;
 	private String accountId;
 	private int productNumber;
 	private String productCategory;
-	
 	private int quantity;
+	private int totalPrice;
+	private String currency;
+	private String validationPeriod;
 	
-	
-	public Product(){}
-	public Product(int rfqId, String accountId, int productNumber, String productCategory, int quantity) {
+	public ProductReturn(){}
+	public ProductReturn(int rfqId, String accountId, int productNumber, String productCategory, int quantity, int totalPrice,
+			String currency, String validateionPeriod) {
 		super();
 		this.rfqId = rfqId;
 		this.accountId = accountId;
 		this.productNumber = productNumber;
 		this.productCategory = productCategory;
 		this.quantity = quantity;
+		this.totalPrice = totalPrice;
+		this.currency = currency;
+		this.validationPeriod = validateionPeriod;
 	}
 	
 	public int getRfqId() {
@@ -49,12 +51,28 @@ public class Product implements Serializable{
 	public void setProductCategory(String productCategory) {
 		this.productCategory = productCategory;
 	}
-	
-	
 	public int getQuantity() {
 		return quantity;
 	}
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int price) {
+		this.totalPrice = price;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getValidationPeriod() {
+		return validationPeriod;
+	}
+	public void setValidationPeriod(String validateionPeriod) {
+		this.validationPeriod = validateionPeriod;
 	}
 }
--- ProductInfo/src/com/assignment/services/ProductService.java
+++ ProductInfo/src/com/assignment/services/ProductService.java
@@ -1,6 +1,11 @@
 package com.assignment.services;
 
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.print.attribute.standard.Media;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -15,6 +20,7 @@ import org.bson.Document;
 
 import com.assignment.databaseconnection.DatabaseConnection;
 import com.assignment.model.Product;
+import com.assignment.model.ProductReturn;
 import com.google.gson.Gson;
 import com.sun.research.ws.wadl.Application;
 
@@ -24,22 +30,21 @@ public class ProductService {
     @GET
     @Path("/{rfqid}/{actid}/{prno}/{prcat}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Product getPrice(@PathParam("rfqid") int rfqid,@PathParam("actid") String actid,
+    public ProductReturn getPrice(@PathParam("rfqid") int rfqid,@PathParam("actid") String actid,
     		@PathParam("prno") int prno,@PathParam("prcat") String pract,@PathParam("qty") int qty) {
     	
     	DatabaseConnection dc = new DatabaseConnection();
     	Document doc = dc.getPrice(rfqid, actid, prno, pract);
     	
-    	Product prod = new Product();
+    	ProductReturn prod = new ProductReturn();
     	
     	prod.setRfqId((int)Double.parseDouble(doc.get("RFQID").toString()));
     	prod.setAccountId(doc.get("AccountID").toString());
     	prod.setProductNumber((int)Double.parseDouble(doc.get("ProductNumber").toString()));
     	prod.setQuantity((int)Double.parseDouble(doc.get("Quantity").toString()));
     	prod.setProductCategory(doc.get("ProductCategory").toString());
-    	prod.setUnitPrice((int)(Double.parseDouble(doc.get("Quantity").toString())*Double.parseDouble(doc.get("Price").toString())));
-    	prod.setValidateionPeriod(doc.get("ValidStartDate").toString());
-        String output =  doc.toString();
+    	prod.setTotalPrice((int)(Double.parseDouble(doc.get("Quantity").toString())*Double.parseDouble(doc.get("Price").toString())));
+    	prod.setValidationPeriod(doc.get("ValidStartDate").toString());
         
         	    return prod;
         	
@@ -48,13 +53,36 @@ public class ProductService {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Product getActualPrice(Product prod){
+    public ProductReturn getActualPrice(Product prod){
+    	Gson serialize = new Gson();
+		String finalOutput = serialize.toJson(prod);
+		System.out.println(finalOutput);
+    	ProductReturn prodReturn = new ProductReturn();
+    	prodReturn.setRfqId(prod.getRfqId());
+    	prodReturn.setAccountId(prod.getAccountId());
+    	prodReturn.setProductCategory(prod.getProductCategory());
+    	prodReturn.setQuantity(prod.getQuantity());
+    	prodReturn.setProductNumber(prod.getProductNumber());
     	DatabaseConnection dc = new DatabaseConnection();
-    	Document doc = dc.getPrice(prod.getRfqId(), prod.getAccountId(), prod.getProductNumber(), prod.getProductCategory());
+    	Document doc = dc.getPrice(prodReturn.getRfqId(), prodReturn.getAccountId(), prodReturn.getProductNumber(), prodReturn.getProductCategory());
     	double dd = Double.parseDouble(doc.get("Price").toString());
-    	prod.setUnitPrice((int)(prod.getQuantity()*Double.parseDouble(doc.get("Price").toString())));
-    	prod.setValidateionPeriod(doc.get("ValidStartDate").toString());
-    	return prod;
+    	prodReturn.setTotalPrice((int)(prodReturn.getQuantity()*Double.parseDouble(doc.get("Price").toString())));
+    	prodReturn.setCurrency(doc.get("PriceUnit").toString());
+    	DateFormat df = new SimpleDateFormat("MM/dd/yyyy"); 
+    	Date startDate =  null;
+    	
+    	    try {
+				startDate = df.parse(doc.get("ValidStartDate").toString());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	    String newDateString = df.format(startDate);
+    	    System.out.println(newDateString);
+    	
+
+    	prodReturn.setValidationPeriod("3 months after manufacturing date: "+newDateString);
+    	return prodReturn;
     }
 
 }
